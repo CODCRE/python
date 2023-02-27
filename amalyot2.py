0937a69f44fcb7a47c6e9bbf58a5dd469dd74cd6
Admin80 <muslimnock@gmail.com>
2023-02-27 17:12:46 +0500

    amalyot2

---
+++ amalyot2.py
@@ -0,0 +1,28 @@
+# ism =['Erkin','Farxod','Akbar']
+# print('Salom',ism[2],'bugun choyhona bormi?\n',ism[0],'choyhonaga boramizmi?')
+# sonlar =[20,30,50,-50,5000,6000]
+# del sonlar[3]
+# sonlar.insert(1,int(8))
+# javob=sonlar[1]+sonlar[5]
+# print(javob)
+# print(sonlar)
+# t_shaxslar ='Shoxrux'
+# z_shaxslar='Ilon Musk'
+# print('men tarihiy shaxslardan',t_shaxslar,'\n va zamonaviy shaxslardan',z_shaxslar,'\n bilan suhbat qilishni xoxlar edim')
+# friends=[]
+# friends.append('shoxrux')
+# friends.append('erkin')
+# friends.append('farxod')
+# friends.append('akbar')
+
+# friends.remove('shoxrux')
+# friends.insert(0, 'botir')
+# friends.insert(3, 'ulug')
+# mehmonlar=[]
+# mehmonlar=friends.pop(2)
+# print(mehmonlar)
+# mehmonlar = []
+# mehmonlar.append(friends.pop(3))
+# mehmonlar.append(friends.pop(-1))
+# mehmonlar.append(friends.pop(0))
+# print("\nKelgan mehmonlar: ", mehmonlar)
